7eacee0156761c49a6b265b5568e5e6c2a83ab8f
foxmessenger <iamliamfox@gmail.com>
2017-04-19 18:39:31 -0500

    bamazaon.js - added an end connection function, nested final inquire function inside second promise, cleaned up spacing of code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,159 +26,144 @@ var inquirer = require('inquirer');
 
 // connecting to the database
 var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: '',
-	database: 'Bamazon'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'Bamazon'
 });
 
 // connect to the server
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log('connected...');
+    if (err) throw err;
+    console.log('connected...');
 });
 
-
+// ending the connection
+var end = function() {
+    connection.end(function(err) {
+        // The connection is terminated now
+    });
+}
 
 // reset console screen
-console.reset = function () {
-	console.log('');
+console.reset = function() {
+    console.log('');
     console.log('');
     console.log('no worries, maybe next time. Goodbye.')
     console.log('');
     console.log('');
-    
-    setTimeout(function () {
-  		return process.stdout.write('\033c');
-	}, 1000);
-  	
+
+    setTimeout(function() {
+        return process.stdout.write('\033c');
+    }, 1000);
+
 };
 
 // start the app
-var startApp = function(){
-	var query = {};
-	var dataList = '';
-	var stock = '';
-	var price = '';
-	initialQuestions(); // this will initialize the app
+function startApp() {
+    var query = {};
+    var dataList = '';
+    var stock = '';
+    var price = '';
+    initialQuestions(); // this will initialize the app
 }
 
 // ============
 // DISPLAY DATA
 // ============
 
-// display mysql data to the screen
-// function displayData() {
-// 	dataList = 'SELECT * FROM products';
-// 	query = connection.query(dataList, function(err, res){
-// 		console.log('');
-// 		console.log('');
-// 		console.log('Welcome to Bamazon! ');
-// 		console.log('');
-// 		console.log('');
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			for (var i = 0; i < res.length; i++) {
-// 				console.log(
-
-// 					`SHOW TABLES; `
-// 					'ITEM ID: ' + res[i].item_id  + '\n' +
-// 					'Product Name: ' + res[i].product_name + '\n' +
-// 					'Department: '+ res[i].department_name + '\n' + 
-// 					'Price: ' + res[i].price + '\n' + 
-// 					/* 'Stock: ' + res[i].stock_quantity + '\n' + */ 
-// 					'====================='
-
-// 				);
-// 			}
-
-// 		initialQuestions();	
-		
-// 		}
-// 	});
-// }
-
 // prompt questions
 function initialQuestions() {
-	dataList = 'SELECT * FROM products';
-	query = connection.query(dataList, function(err, res){
-		if (err) throw err;
-		inquirer.prompt([
-			{
-				name: 'id',
-				type: 'list',
-				choices: function() {
-			        var choiceArray = [];
-			        for (var i = 0; i < res.length; i++) {
-			        	choiceArray.push(res[i].product_name);
-			        }
-			        
-			        return choiceArray;
-	        	},
-				message: 'Choose the ID of product you\'d like to purchase: '
-				},
-		// ]).then(function(idNumber) {
-		// 	var choice = query._results[0][idNumber.id].product_name;
-		// 	console.log(choice);
-		// 	inquirer.prompt([
-				{
-					type: 'input',
-					message: 'How many would you like to purchase? ',
-					name: 'amount'
-				}
-			
-			]).then(function(answer){
-				var chosenItem;
-			    for (var i = 0; i < res.length; i++) {
-			    	if (res[i].item_name === answer.choice) {
-			    		chosenItem = res[i];
-			    	}
-			    }
 
-				stock = query._results[0][2].stock_quantity;
-				price = query._results[0].price;
-				console.log(query._results[0][chosenItem]);
-				console.log(stock);
+    dataList = 'SELECT * FROM products';
 
-				if (answer.amount <= stock) {
-					var updateFrom = 'UPDATE products SET stock_quantity = ?';
-					connection.query(updateFrom, [answer.amount], function(err, res){
-						if (err) {
-							console.log(err);
-						} else {
-								console.log('That will be ' + '$' + price * answer.amount + '.');
-								console.log('there are ' + stock + ' left');
-						}
-					});
-					
+    query = connection.query(dataList, function(err, res) {
 
-				} else {
-					console.log('Sorry, we don\'t have enough')
-				}
+        if (err) throw err;
+            inquirer.prompt([
+            	{
+                    name: 'id',
+                    type: 'list',
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name);
+                        }
+                        
+                        return choiceArray;
+                    
+                	},
 
-			}).then(function(){
-					inquirer.prompt([
-					{
-						type: 'confirm',
-						message: 'Would you like to purchase another item?',
-						name: 'confirm',
-						default: true				
-					}
-				]).then(function(choice){
-					if (choice.confirm) {
-						startApp();
+                    message: 'Choose the product you\'d like to purchase: '
 
-					} else {
+                },
+                {
+                    type: 'input',
+                    message: 'How many would you like to purchase? ',
+                    name: 'amount',
+                    validate: function(val) {
+                        if (isNaN(val) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
 
-						console.reset();
-					}
-				})
-			})
+                    // WE WANT TO GRAB THIS INFORMATION AND MODIFY OUR TABLES
 
-		// });
-	});
+            ]).then(function(answer) {
+
+                var chosenItem;
+
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.id) {
+                        chosenItem = res[i];
+                    }
+                }
+
+                stock = chosenItem.stock_quantity;
+                price = chosenItem.price;
+
+                console.log(answer.id);
+                console.log('in stock: ' + stock);
+                console.log('requested ' + answer.amount);
+
+                if (answer.amount <= stock) {
+                    var updateFrom = 'UPDATE products SET stock_quantity = ?';
+                    
+                    connection.query(updateFrom, [(stock - answer.amount)], function(err, res) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log((stock - answer.amount) + ' left in stock.');
+                            console.log('That will be ' + '$' + (price * answer.amount) + '.');
+                        }
+                        answerAmountQuery()
+                        // after
+                        function answerAmountQuery() {
+                            inquirer.prompt([{
+                                type: 'confirm',
+                                message: 'Would you like to purchase another item?',
+                                name: 'confirm',
+                                default: true
+                        	}]).then(function(choice) {
+                                if (choice.confirm) {
+                                    startApp();
+                                } else {
+                                    console.reset();
+                                    end();
+                                }
+                            })
+                            
+                        } 
+
+                    })
+            	} else {
+	            	console.log('Sorry, we don\'t have enough');
+	            }
+        })
+	})
 }
 
 startApp();
